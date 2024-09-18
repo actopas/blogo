2a14841dbdec334c8033603a50dd53b2a277f0fe
Actopas <fishmooger@gmail.com>
2024-09-19 04:37:42 +0800

    chore: seo optimize update

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -1,28 +1,20 @@
 const siteUrl = process.env.SITE_URL;
+console.log('Site URL:', siteUrl);
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl,
-  // 以/admin/*的路由不生成sitemap
   exclude: ['/admin/*', '/auth/*', '/server-sitemap.xml'],
-  generateRobotsTxt: true, // (optional)
+  generateRobotsTxt: true,
   generateIndexSitemap: false,
-  // ...other options
   robotsTxtOptions: {
     policies: [
       {
         userAgent: '*',
+        allow: '/', // 允许爬取网站的所有页面
       },
       {
         userAgent: '*',
-        disallow: ['/admin/', '/admin/*'],
-      },
-      {
-        userAgent: '*',
-        disallow: ['/auth/', '/auth/*'],
-      },
-      {
-        userAgent: '*',
-        allow: '/',
+        disallow: ['/admin/', '/auth/', '/admin/*', '/auth/*'], // 禁止爬取/admin和/auth相关页面
       },
     ],
   },
