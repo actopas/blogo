db6f3d3f8cbad0c194497045a6c0365714d4a27f
Actopas <fishmooger@gmail.com>
2024-10-05 19:55:15 +0800

    chore: chore: add db.text in blog model

---
+++ prisma/migrations/20241005115445_increase_description_length/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `Blog` MODIFY `bodyEN` TEXT NOT NULL,
+    MODIFY `bodyZH` TEXT NOT NULL,
+    MODIFY `descriptionEN` TEXT NOT NULL,
+    MODIFY `descriptionZH` TEXT NOT NULL;
