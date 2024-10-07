9c8cb307a9028910584ed1eede4d46dad8a92745
Actopas <fishmooger@gmail.com>
2024-10-08 00:56:41 +0800

    feat: first page loading

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,7 @@
 import { unstable_setRequestLocale } from 'next-intl/server';
 
+import { LoadingAnimation } from '@/components/loading-animation';
+import { LoadingProvider } from '@/components/loading-context';
 import { ScrollIndicator } from '@/components/scroll-indicator';
 
 import { BlogList, getPinnedBlogs } from '@/features/blog';
@@ -19,16 +21,20 @@ export default async function Page({
   ]);
   const { projects } = projectsData;
   const { blogs, uvMap } = blogsData;
+
   return (
-    <div>
-      <div className="h-[calc(100vh-64px)] grid place-content-center relative">
-        <HeroSection />
+    <LoadingProvider>
+      <LoadingAnimation />
+      <div>
+        <div className="h-[calc(100vh-64px)] grid place-content-center relative">
+          <HeroSection />
+        </div>
+        <div className="grid place-content-center absolute bottom-8 md:bottom-12 inset-x-0">
+          <ScrollIndicator />
+        </div>
+        <ProjectTimeline projects={projects} />
+        <BlogList blogs={blogs} uvMap={uvMap} />
       </div>
-      <div className="grid place-content-center absolute bottom-8 md:bottom-12 inset-x-0">
-        <ScrollIndicator />
-      </div>
-      <ProjectTimeline projects={projects} />
-      <BlogList blogs={blogs} uvMap={uvMap} />
-    </div>
+    </LoadingProvider>
   );
 }
